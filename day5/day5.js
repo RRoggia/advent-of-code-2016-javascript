175eee549c9e0bc53e4c3bd90ba66268b3f400c3
rroggia <renanzr@gmail.com>
2016-12-13 23:37:42 -0200

    solve second puzzle of day 5

--- day5/day5.js
+++ day5/day5.js
@@ -1,18 +1,27 @@
-var password = "";
+var password = [];
 
 var puzzleInput ="wtnhxymk";
 var increassingIndex = 0;
+console.log("start");
+var passwordLenghtWithoutUndefined=0;
 
-while(password.length < 8){
+while(passwordLenghtWithoutUndefined < 8){
 	var combination = puzzleInput + increassingIndex;
 
 	var hash = CryptoJS.MD5(combination);
 	var fiveDigitsHexadecimalRepresentation = hash.toString().substring(0,5);
 
-	if(fiveDigitsHexadecimalRepresentation == "00000")
-		password += hash.toString().substring(5,6);
+	if(fiveDigitsHexadecimalRepresentation == "00000"){
+		var possiblePasswordIndex = parseInt(hash.toString().substring(5,6));
+		if( possiblePasswordIndex !== NaN && possiblePasswordIndex < 8 ){
+			if(!password[possiblePasswordIndex]){
+				password[possiblePasswordIndex] = hash.toString().substring(6,7);
+				passwordLenghtWithoutUndefined++;
+			}
+		}
+	}
 
 	increassingIndex++;
 }
 
-console.log(password);
+console.log(password.toString().replace(/,/g,""));
