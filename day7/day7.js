32a6d84ab984464be085866c7044b7c0093db3dc
Rroggia <renanzr@gmail.com>
2016-12-15 17:29:09 -0200

    add 2nd puzzle for day 7)

--- day7/day7.js
+++ day7/day7.js
@@ -1,35 +1,56 @@
 var ipAddresses = getInputs();
-var supportTLS = 0;
+var supportSSL = 0;
 
 for (var i = 0; i < ipAddresses.length; i++) {
 	var ipAddress = ipAddresses[i];
 
-	var hypernets = getHypernets(ipAddress);
+	var address = getIPWithoutHypernet(ipAddress);
+	var abas = getXYX(address);
 
-	if(hasABBA(hypernets)){
+	if(abas === null){
+		continue;
+	}
+		
+	var hypernets = getHypernets(ipAddress);
+	var babs = getXYX(hypernets);
+	
+	if(babs === null){
 		continue;
 	}
 
-	var address = getIPWithoutHypernet(ipAddress);
-
-	if(hasABBA(address)){
-		supportTLS++;
+	if(supportsSSL(abas,babs)){
+		supportSSL++;	
 	}
 
 };
 
-console.log("There are ", supportTLS, " ip address that support TLS");
+console.log("There are ", supportSSL, " ip address that support TLS");
 
 function getHypernets(address){
 	return address.match(/\[[a-z]*\]/g);
 }
 
-function hasABBA(address){
+function getXYX(address){
+	var aba = [];
 	for (var i = 0; i < address.length; i++) {
 		var hypernet = address[i];	
 
 		for (var j = 0; j < hypernet.length; j++) {
-			if(hypernet[j] !== hypernet[j+1] && hypernet[j] === hypernet[j+3] && hypernet[j+1] === hypernet[j+2]){
+			if(hypernet[j] !== hypernet[j+1] && hypernet[j] === hypernet[j+2]){
+				aba.push(hypernet[j] + hypernet[j+1] + hypernet[j+2]);
+			}
+		}
+	}
+	return aba;
+}
+
+function supportsSSL(abas, babs){
+	for (var i = 0; i < abas.length; i++) {
+		var aba = abas[i];
+		for (var j = 0; j < babs.length; j++) {
+			var bab = babs[j];
+
+			if(aba[0] === bab[1] && aba[1] === bab[0]){
 				return true;
 			}
 		}
