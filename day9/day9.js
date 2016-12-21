0675683687b2b1d8e4ff9c46235538dd2f62fde8
Rroggia <renanzr@gmail.com>
2016-12-21 17:24:31 -0200

    1st attempt 2nd puzzle day 9 with brute force (didnot work)

--- day9/day9.js
+++ day9/day9.js
@@ -1,29 +1,42 @@
 var compressedFile = getInputs();
+
+debugger;
 	
-var decompressedFile = "";
+do {
 
-var hasDataToDecompress = true;
+	var hasDataToDecompress = true;
+	var decompressedFile = "";
 
-while(hasDataToDecompress){
-	if(!compressedFile){
-		hasDataToDecompress = false;
-		continue;
+    while(hasDataToDecompress){
+		if(!compressedFile || compressedFile.match(/\((.?){4}x.(.?){3}\)/) === null){
+			decompressedFile += compressedFile;
+			compressedFile = "";
+			hasDataToDecompress = false;
+			continue;
+		}
+
+		var nextMarker = compressedFile.match(/\((.?){4}x.(.?){3}\)/);
+
+		for (var i = 0; i < nextMarker.index; i++) {
+			decompressedFile += compressedFile[i];
+		}
+
+		var marker = new Marker(nextMarker[0]);
+		console.log(marker);
+
+		var stringToRepeat = compressedFile.substring(nextMarker.index + marker.length, nextMarker.index + marker.length + marker.subsequentChar);
+
+		for (var j = 0; j < marker.repeatTimes; j++) {
+			decompressedFile += stringToRepeat;
+		}
+
+		compressedFile = compressedFile.substring(nextMarker.index + marker.length + marker.subsequentChar, compressedFile.length);
 	}
 
-	var nextMarker = compressedFile.match(/\((.?){4}x.(.?){3}\)/);
-
-	var marker = new Marker(nextMarker[0]);
-	console.log(marker);
-
-	var stringToRepeat = compressedFile.substring(marker.length, marker.length + marker.subsequentChar);
-
-	for (var j = 0; j < marker.repeatTimes; j++) {
-		decompressedFile += stringToRepeat;
-	}
-
-	compressedFile = compressedFile.substring(marker.length + marker.subsequentChar, compressedFile.length);
+	compressedFile = decompressedFile;
 
 }
+while (decompressedFile.match(/\((.?){4}x.(.?){3}\)/));
 
 console.log(decompressedFile.length);
 
