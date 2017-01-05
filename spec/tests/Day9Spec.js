a56a883bf06e12f6ac4ca3c6b16651f8920a0ba6
rroggia <renanzr@gmail.com>
2017-01-05 12:14:31 -0200

    test if the marker repeat times is correct

--- spec/tests/Day9Spec.js
+++ spec/tests/Day9Spec.js
@@ -1,17 +1,21 @@
 describe("Day Nine", function() {
 
 	var Decompressor = require('../../day9/Decompressor');
-	console.log(Decompressor);
+
+	var decompressor = null;
+	beforeEach(function(){
+		var compressedData = 'A(1x5)BC';
+		decompressor = new Decompressor(compressedData);
+	});
 
 	it("should find a marker", function() {
-		var compressedData = 'A(1x5)BC';
-		
-		var decompressor = new Decompressor(compressedData);
-
 		var marker = decompressor.getNextMarker();
-
 		expect(marker).not.toBe(null);
+	});
 
+	it("Marker should know how many times to repeat a string", function(){
+		var marker = decompressor.getNextMarker();
+		expect(marker.repeatTimes).toBe(5);
 	});
 
 
