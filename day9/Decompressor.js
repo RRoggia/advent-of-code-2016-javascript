153be2ad2f162ab619af8864def762670b2a1a2c
rroggia <renanzr@gmail.com>
2017-01-05 13:35:37 -0200

    encapsulate changeNextMarkerPosition

--- day9/Decompressor.js
+++ day9/Decompressor.js
@@ -5,20 +5,25 @@ function Decompressor(compressedData){
 };
 
 Decompressor.prototype.getNextMarker = function(){
-	var compressedDataWithoutLastMarker = this.compressedData.substring(this.compressedDataLastPosition, this.compressedData.length);
-	var compressedMarker = compressedDataWithoutLastMarker.match(/\((.?){4}x.(.?){3}\)/);
+	
+	var compressedMarker = this.getNextMarkerFromCompressedData();
 
 	if(!compressedMarker){
 		return null;	
 	}else{
 		var marker = new Marker(compressedMarker[0]);	
-		this.compressedDataLastPosition += compressedMarker.index + marker.subsequentChar + marker.length;
+		this.changeNextMarkerPosition(compressedMarker.index, marker);
 		return marker;
 	}
 };
 
-Decompressor.prototype.getRemainingString = function(){
+Decompressor.prototype.getNextMarkerFromCompressedData = function(){
+	var compressedDataWithoutLastMarker = this.compressedData.substring(this.compressedDataLastPosition, this.compressedData.length);
+	return  compressedDataWithoutLastMarker.match(/\((.?){4}x.(.?){3}\)/);
+};
 
+Decompressor.prototype.changeNextMarkerPosition = function(currentMarkerInitialPosition, currentMarker){
+	this.compressedDataLastPosition += currentMarkerInitialPosition + currentMarker.subsequentChar + currentMarker.length;
 };
 
 module.exports = Decompressor;
