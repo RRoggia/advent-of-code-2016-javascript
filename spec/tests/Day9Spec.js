dea5e1f16fb84cb6ac6de52b7b2b21c1b3799b81
rroggia <renanzr@gmail.com>
2017-01-24 13:50:43 -0200

    change logixc

--- spec/tests/Day9Spec.js
+++ spec/tests/Day9Spec.js
@@ -32,11 +32,28 @@ describe("Marker", function() {
 		expect(length).toBe(241920);
 	});
 
+	it("should contains next compressed Data", function(){
+		compressedData = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRST(18x9)(3x2)TWO(5x7)SEVEN';
+		marker = new Marker(compressedData);
+		expect(marker.nextCompressedData).toBe('(18x9)(3x2)TWO(5x7)SEVEN');
+	});
+
 	it("should add same level markers", function(){
-		compressedData = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN';
+		compressedData = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRST';
 		marker = new Marker(compressedData);
 		var length = marker.getDecompressedDataLength();
+		expect(length).toBe(75);
+	});
+
+	it("should multiply or add depending on next Marker", function(){
+		compressedData = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRST(18x9)(3x2)TWO(5x7)SEVEN';
+		marker = new Marker(compressedData);
+		var teste = '';
+		var length = marker.getDecompressedDataLength();
+		console.log(teste);	
 		expect(length).toBe(445);
+
+
 	});
 
 });
